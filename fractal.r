325bce1e69b8fc4620218e01098c871cf7bbd8c0
pauljabernathy <pauljabernathy@gmail.com>
2014-06-10 07:27:42 -0400

    updates to fractal.r

--- fractal.r
+++ fractal.r
@@ -1,4 +1,28 @@
-sapierskyCarpet <- function(depth = 4) {
+sierpinskiTriangle = function(numPoints = 10000) {
+  currentX <- runif(1);
+  currentY <- runif(1);
+  
+  xCorners <- c(0, 1, .5);
+  yCorners <- c(0, 0, 3^.5 / 2);
+  
+  xPoints <- vector();
+  yPoints <- vector();
+  
+  xPoints <- c(xPoints, currentX);
+  yPoints <- c(yPoints, currentY);
+  
+  for(i in 1:numPoints) {
+    corner <- randomInt(3);
+    currentX <- (currentX + xCorners[corner]) / 2;
+    currentY <- (currentY + yCorners[corner]) / 2;
+    
+    xPoints <- c(xPoints, currentX);
+    yPoints <- c(yPoints, currentY);
+  }
+  plot(xPoints, yPoints, pch=20);
+}
+
+sierpinskiCarpet <- function(depth = 4, shouldReturn = FALSE) {
   if(depth < 1) {
     stop("can't have a depth less than 1");
   }
@@ -13,14 +37,14 @@ sapierskyCarpet <- function(depth = 4) {
         iEnd <- iStart + width / 3 - 1;
         jStart <- 1 + (width / 3) * j;
         jEnd <- jStart + width / 3 - 1;
-        #print(paste(iStart, iEnd, jStart, jEnd, sep=" "));
-        #print(carpet[iStart:iEnd, jStart:jEnd]);
-        carpet[iStart:iEnd, jStart:jEnd] <- sapierskyCarpet(depth - 1);
-        #print(sapierskyCarpet(depth - 1));
+        carpet[iStart:iEnd, jStart:jEnd] <- sierpinskiCarpet(depth - 1, TRUE);
         }
       }
     }
   }
-  image(carpet, col=c("white", "black"));
-  return(carpet);
+  if(shouldReturn) {
+    return(carpet);
+  } else {
+    image(carpet, col=c("white", "black"));
+  }
 }
