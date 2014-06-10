885f7926c61a177dd5e9d2bd4d48f834cc523534
pauljabernathy <pauljabernathy@gmail.com>
2014-06-10 07:27:16 -0400

    education.r

--- education.R
+++ education.R
@@ -3,13 +3,13 @@ partitionOneNumProblems = function(sum, filename = 'partitions.csv') {
   nums = sample(sum, sum, replace=FALSE);
   otherNums = sum - nums;
   for(i in 1:sum) {
-    #print(paste(sum, " = ", nums[i], " + ", otherNums[i]));
+    #print(paste(sum, " = ", nums[i], " + ", otherNums[i], sep=","));
     
   }
   problems = data.frame(sum, '=', nums, rep('+', sum), otherNums);
   names(problems) = c('sum', 'equals','first', 'plus', 'second');
   problems;
-  write.csv(problems, filename);
+  #write.csv(problems, filename);
 }
 
 #sumsVector is a vector of integers; need to do partitioning for each number in it
